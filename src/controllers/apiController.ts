383aa0d18f2cece6469bbbdf5f9466f22a82dd04
Ana.Sousa <anasousa314@outlook.com>
2021-11-15 17:44:02 -0300

    aula09 criando uma frase nova

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -7,13 +7,24 @@ export const ping = (req: Request,res:Response)=>{
 }
 
 export const random = (req: Request,res:Response)=>{
-    let nRand: number = Math.floor(Math.random() * 10);
+  let nRand: number = Math.floor(Math.random() * 10);
   
-    res.json({number: nRand});
-  }
+  res.json({number: nRand});
+}
 
-  export const nome = (req: Request,res:Response)=>{
-    let nome: string = req.params.nome;
+export const nome = (req: Request,res:Response)=>{
+  let nome: string = req.params.nome;
   
-    res.json({nome: `Você enviou o nome ${nome}`});
-  }
+  res.json({nome: `Você enviou o nome ${nome}`});
+}
+
+export const createPhrase = async (req: Request,res:Response)=>{
+  
+  let {author, txt} = req.body;
+
+  let newPhrase = await Phrase.create({
+    author,txt
+  });
+
+  res.json({id: newPhrase.id,author,txt})
+}
